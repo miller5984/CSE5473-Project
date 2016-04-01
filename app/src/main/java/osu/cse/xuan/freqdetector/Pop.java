6e16b321fb30be7cfcd3af4f00480bb80eb0e1ab
miller5984 <d51fmc78z>
2016-04-01 11:05:42 -0400

    Recording portion completed; testing required

--- app/src/main/java/osu/cse/xuan/freqdetector/Pop.java
+++ app/src/main/java/osu/cse/xuan/freqdetector/Pop.java
@@ -1,20 +1,25 @@
 package osu.cse.xuan.freqdetector;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+
 
 public class Pop extends Activity implements View.OnClickListener{
 
 
-    Button record, send, restart, pause;
-    TextView boxmessage;
+    Button record, send, play, stop;
+    TextView boxMessage;
+    private MediaRecorder myAudioRecorder;
+    private String outputFile = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -23,17 +28,24 @@ public class Pop extends Activity implements View.OnClickListener{
 
 
         record = (Button)findViewById(R.id.record);
-        pause = (Button)findViewById(R.id.pause);
+        stop = (Button)findViewById(R.id.stop);
         send = (Button)findViewById(R.id.send);
-        restart = (Button)findViewById(R.id.restart);
-        boxmessage = (TextView)findViewById(R.id.boxmessage);
+        play = (Button)findViewById(R.id.play);
+        boxMessage = (TextView)findViewById(R.id.boxmessage);
 
+        stop.setEnabled(false);
+        play.setEnabled(false);
 
         record.setOnClickListener(this);
-        pause.setOnClickListener(this);
-        restart.setOnClickListener(this);
+        stop.setOnClickListener(this);
+        play.setOnClickListener(this);
         send.setOnClickListener(this);
 
+        myAudioRecorder = new MediaRecorder();
+        myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        myAudioRecorder.setOutputFile(outputFile);
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
@@ -51,10 +63,29 @@ public class Pop extends Activity implements View.OnClickListener{
             case R.id.record:
                 Toast.makeText(getApplicationContext(), "You are recording!",
                         Toast.LENGTH_SHORT).show();
+                try {
+                    myAudioRecorder.prepare();
+                    myAudioRecorder.start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                catch(IllegalStateException e){
+                    e.printStackTrace();
+                }
+
+                record.setEnabled(false);
+                stop.setEnabled(true);
                 break;
-            case R.id.pause:
-                Toast.makeText(getApplicationContext(), "You have paused your recording!",
+            case R.id.stop:
+                Toast.makeText(getApplicationContext(), "You have made your recording!",
                         Toast.LENGTH_SHORT).show();
+                myAudioRecorder.stop();
+                myAudioRecorder.release();
+                myAudioRecorder = null;
+
+                stop.setEnabled(false);
+                play.setEnabled(true);
+
                 break;
 
             case R.id.send:
@@ -62,10 +93,21 @@ public class Pop extends Activity implements View.OnClickListener{
                         Toast.LENGTH_SHORT).show();
                 break;
 
-            case R.id.restart:
-                Toast.makeText(getApplicationContext(), "You are restarting your recording!",
+            case R.id.play:
+                MediaPlayer m = new MediaPlayer();
+                try{
+                    m.setDataSource(outputFile);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try{
+                    m.prepare();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                m.start();
+                Toast.makeText(getApplicationContext(), "Playing Audio",
                         Toast.LENGTH_SHORT).show();
-
                 break;
         }
     }
